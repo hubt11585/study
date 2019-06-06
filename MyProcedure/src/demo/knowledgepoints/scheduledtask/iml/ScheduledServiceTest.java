6f54b00dfca3ab08d23742a6eecaae882ff0fc17
AccelerateWorld <1050750201@qq.com>
2019-06-06 09:05:01 +0800

    题目9答案

--- MyProcedure/src/demo/knowledgepoints/scheduledtask/iml/ScheduledServiceTest.java
+++ MyProcedure/src/demo/knowledgepoints/scheduledtask/iml/ScheduledServiceTest.java
@@ -1,30 +1,8 @@
 package demo.knowledgepoints.scheduledtask.iml;
 
-import demo.knowledgepoints.scheduledtask.inf.TaskServcesInf;
-
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-
 public class ScheduledServiceTest {
 
     public static void main(String[] args) {
-
-        /*Class<?> cls = null;
-        try {
-            cls = Class.forName("demo.knowledgepoints.scheduledtask.iml.TaskServcesIml");
-
-            TaskServcesIml taskServcesIml = (TaskServcesIml) cls.newInstance();
-            taskServcesIml.Test1();
-
-
-        Method getMoney = cls.getMethod("Test1");//得到方法对象
-            getMoney.invoke(cls.newInstance(),null);
-        getMoney.invoke(cls);//调用借钱方法，得到返回值
-
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }*/
         ScheduledServiceIml scheduledServiceIml = new ScheduledServiceIml();
         try {
             scheduledServiceIml.init();
---
+++ MyProcedure/src/demo/practice/Subject9.java
@@ -0,0 +1,48 @@
+package demo.practice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/***
+ * 【程序9】
+ * 题目：一个数如果恰好等于它的因子之和，这个数就称为”完数”。例如6=1＋2＋3.编程找出1000以内的所有完数。
+ */
+public class Subject9 {
+    public static void main(String[] args) {
+        for (int i = 0; i <1000 ; i++) {
+            perfectNum(i);
+        }
+    }
+
+    /**
+     * 判断是否是完数
+     */
+    public static void perfectNum(int num){
+        List<Integer> list = decompose(num);
+        int sum = 0;
+        for(int i :list){
+            sum = sum + i;
+        }
+        if(num == sum){
+            System.out.println("完数："+num);
+        }
+    }
+
+    /**
+     * 分解质因数
+     * @param num
+     * @return
+     */
+    public static List decompose(int num){
+        List<Integer> list = new ArrayList();
+        list.add(1);
+        int tmp = num/2;
+        for(int i=2;i <= tmp ;i++){
+            if(num%i == 0){
+                list.add(i);
+                continue;
+            }
+        }
+        return list;
+    }
+}
