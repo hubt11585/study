e2a518376937d15bad2565667f784a80dfaac764
AccelerateWorld <1050750201@qq.com>
2019-06-15 10:00:58 +0800

    题目24的答案

---
+++ MyProcedure/src/demo/practice/Subject24.java
@@ -0,0 +1,33 @@
+package demo.practice;
+
+import java.util.Scanner;
+
+/**
+ * 【程序24】
+ * 题目：给一个不多于5位的正整数，要求：一、求它是几位数，二、逆序打印出各位数字。
+ */
+public class Subject24 {
+
+    public static void main(String[] args) {
+        System.out.println("请输入需要分析的正整数：");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        analysisInt(num);
+    }
+
+    /**
+     * 分析正整数
+     * @param num
+     */
+    private static void analysisInt(int num) {
+        String tmpStr = String.valueOf(num);
+        char[] arrStr =tmpStr.toCharArray();
+        System.out.println("该正整数是"+arrStr.length+"位数。");
+        System.out.println("倒序打印为：");
+        for (int i = arrStr.length-1; i >= 0; i--) {
+            System.out.print(arrStr[i]+" ");
+        }
+    }
+
+
+}
