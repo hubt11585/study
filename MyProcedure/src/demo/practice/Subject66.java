a04abe0b21659774081ad7d95053562f612606f0
AccelerateWorld <1050750201@qq.com>
2019-07-31 19:51:33 +0800

    subject66

--- MyProcedure/src/demo/practice/Subject66.java
+++ MyProcedure/src/demo/practice/Subject66.java
@@ -1,5 +1,15 @@
 package demo.practice;
 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ *
+ * 给定一个链表(单项链表)，删除链表的倒数第 n 个节点，并且返回链表的头结点。
+ */
 public class Subject66 {
     public static void main(String[] args) {
         ListNode listNode0 = new ListNode(1);
@@ -11,12 +21,47 @@ public class Subject66 {
         listNode1.next = listNode2;
         listNode2.next = listNode3;
         listNode3.next = listNode4;
-        System.out.println(removeNthFromEnd(listNode0,2));
+        System.out.println(new Subject66().removeNthFromEnd(listNode0,1));
     }
 
-    public static ListNode removeNthFromEnd(ListNode head, int n) {
+    //公共变量
+    public static ListNode listNodeTmp;
 
+    /**
+     * 删除倒数N的节点
+     * @param head
+     * @param n
+     * @return
+     */
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        listNodeTmp = null;
+        if(head == null){
+            return head;
+        }
+        int num = nodeNum(head,n);
+        if(num == n){
+            return head.next;
+        }
         return head;
     }
 
+    /**
+     * 递归获取节点位置
+     * @param head
+     * @param n
+     * @return
+     */
+    public int nodeNum(ListNode head,int n){
+        int tmp = 0;
+        if(head != null){
+            tmp = 1+ nodeNum(head.next,n);
+            if(tmp == n +1){
+                head.next = listNodeTmp;
+            }
+            if(tmp == n-1){
+                listNodeTmp = head;
+            }
+        }
+        return tmp;
+    }
 }
