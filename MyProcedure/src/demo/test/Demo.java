69c704647c6a98ec087b6f585a90947b950221c3
hubt11585 <1050750201@qq.com>
2019-08-07 22:53:20 +0800

    1

--- MyProcedure/src/demo/test/Demo.java
+++ MyProcedure/src/demo/test/Demo.java
@@ -1,8 +1,15 @@
 package demo.test;
 
-
 public class Demo {
+    private String name;
+    private int age;
+
+    public Demo(){
+        name = "微学院";
+        age = 3;
+    }
     public static void main(String[] args){
-        Dog dog = new Dog();
+        Demo demo = new Demo();
+        System.out.println(demo.name + "的年龄是" + demo.age);
     }
 }
