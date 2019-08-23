821d20875ab0fad75c18b27fcf9d8ec4d9162779
AccelerateWorld <1050750201@qq.com>
2019-08-23 08:49:50 +0800

    subject77

---
+++ MyProcedure/src/demo/practice/Subject77.java
@@ -0,0 +1,8 @@
+package demo.practice;
+
+/**
+ * 给定一个字符串s和一些长度相同的单词words。找出 s 中恰好可以由words 中所有单词串联形成的子串的起始位置。
+ * 注意子串要与words 中的单词完全匹配，中间不能有其他字符，但不需要考虑words中单词串联的顺序。
+ */
+public class Subject77 {
+}
