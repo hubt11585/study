6fdf82d0c262b29a8c674ee5497eb008dfc31d0d
hubt11585 <1050750201@qq.com>
2019-08-28 22:42:38 +0800

    subject78

--- MyProcedure/src/demo/practice/Subject78.java
+++ MyProcedure/src/demo/practice/Subject78.java
@@ -12,14 +12,52 @@ package demo.practice;
 public class Subject78 {
 
     public static void main(String[] args) {
-
+        int[] arr = new int[]{2,3,1,3,3};
+        new Subject78().nextPermutation(arr);
+        System.out.println(arr);
     }
 
     /**
-     *
+     * 下一个最大值
      * @param nums
      */
     public void nextPermutation(int[] nums) {
-
+        int lengths = nums.length;
+        int size = -1;
+        for (int i = lengths-1; i >= 0; i--) {
+            if(i-1 >= 0 && nums[i-1] < nums[i]){
+                size = i-1;
+                break;
+            }
+        }
+        //如果没有最大的值了
+        if(size == -1){
+            for (int i = 0 ,j= lengths-1; i <= j ; i++,j--) {
+                int tmp = 0;
+                tmp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = tmp;
+            }
+        }else{ //处理size后边的数据，重新整理成一个最小数组。
+            //找到比size位置大的数中的最小数。
+            int tmp = nums[size+1];
+            int sizeExchange = size+1;
+            for (int i = size+1; i < lengths ; i++) {
+                //这里可以优化
+                if(nums[i] <= tmp && nums[i] > nums[size]){
+                    tmp = nums[i];
+                    sizeExchange = i;
+                }
+            }
+            nums[sizeExchange] = nums[size];
+            nums[size] = tmp;
+            //剩余数据重新重小到大排序
+            for (int i = size+1 ,j= lengths-1; i <= j ; i++,j--) {
+                int tmp0 = 0;
+                tmp0 = nums[i];
+                nums[i] = nums[j];
+                nums[j] = tmp0;
+            }
+        }
     }
 }
---
+++ MyProcedure/src/demo/practice/Subject79.java
@@ -0,0 +1,15 @@
+package demo.practice;
+
+/**
+ * 给定一个只包含 '(' 和 ')' 的字符串，找出最长的包含有效括号的子串的长度。
+ */
+public class Subject79 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int longestValidParentheses(String s) {
+        return 0;
+    }
+}
