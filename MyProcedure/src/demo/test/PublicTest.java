c867449973695e83e8558010ad0883c6ea74ee8a
AccelerateWorld <1050750201@qq.com>
2019-08-28 20:05:11 +0800

    subject78

--- MyProcedure/src/demo/test/PublicTest.java
+++ MyProcedure/src/demo/test/PublicTest.java
@@ -4,10 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PublicTest {
-    Integer a;
     public static void main(String[] args) {
-
-        System.out.println("①不能转换");
-
+        int i = 3;
+        switch (i) {
+            case 1:
+                System.out.println("正常");
+                break;
+            case 2:
+                System.out.println("正常");
+                break;
+            case 5:
+                System.out.println("正常");
+                break;
+            default:
+                assert false:"i的值无效";       //如果i的值不是你想要的，程序就警告退出
+        }
+        System.out.println("如果断言正常，我就被打印");
     }
 }
