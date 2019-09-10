8491e808991cce0e9a83b7c4eec9b3c30781dd95
AccelerateWorld <1050750201@qq.com>
2019-09-10 20:23:16 +0800

    subject81

--- MyProcedure/src/demo/practice/Subject81.java
+++ MyProcedure/src/demo/practice/Subject81.java
@@ -7,7 +7,8 @@ package demo.practice;
  */
 public class Subject81 {
     public static void main(String[] args) {
-
+        int[] arr = new int[]{1};
+        System.out.println(new Subject81().searchRange(arr,1));
     }
 
     /**
@@ -17,6 +18,43 @@ public class Subject81 {
      * @return
      */
     public int[] searchRange(int[] nums, int target) {
-        return new int[]{0,0};
+        int left = 0;
+        int right = nums.length-1;
+        int pivot = -1;
+        boolean flag = false;
+        while (left <= right) {
+            pivot = (left + right) / 2;
+            if (nums[pivot] == target) {
+                flag = true;
+                break;
+            } else {
+                if (target < nums[pivot])
+                    right = pivot - 1;
+                else
+                    left = pivot + 1;
+            }
+        }
+        if(!flag){
+            pivot = -1;
+        }
+        if(pivot != -1){
+            int leftTmp = pivot;
+            int rightTmp = pivot;
+            while(leftTmp >= 0){
+                leftTmp = leftTmp-1;
+                if(leftTmp < 0 || nums[leftTmp] != target){
+                    break;
+                }
+            }
+            while(rightTmp <=  nums.length-1){
+                rightTmp = rightTmp+1;
+                if(rightTmp > nums.length-1  ||nums[rightTmp] != target){
+                    break;
+                }
+            }
+            return new int[]{leftTmp+1,rightTmp-1};
+        }else{
+            return new int[]{-1,-1};
+        }
     }
 }
---
+++ MyProcedure/src/demo/practice/Subject82.java
@@ -0,0 +1,16 @@
+package demo.practice;
+
+/**
+ * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+ */
+public class Subject82 {
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1,3,4,5,6,7};
+        System.out.println(new Subject82().searchInsert(arr,3));
+    }
+
+    public int searchInsert(int[] nums, int target) {
+        return 0;
+    }
+}
