2699d106e85ada9c55328e44272eccf7d52f4ac9
AccelerateWorld <1050750201@qq.com>
2019-09-25 19:52:21 +0800

    subject85

--- MyProcedure/src/demo/practice/Subject83.java
+++ MyProcedure/src/demo/practice/Subject83.java
@@ -1,9 +1,5 @@
 package demo.practice;
 
-import java.util.ArrayList;
-import java.util.List;
-
-
 /**
  * 判断一个9x9 的数独是否有效。只需要根据以下规则，验证已经填入的数字是否有效即可。
  * 数字1-9在每一行只能出现一次。
@@ -26,50 +22,34 @@ public class Subject83 {
     }
 
     public boolean isValidSudoku(char[][] board) {
-        //校验行
+        int[] rowCnt = new int[9];
+        int[] colCnt = new int[9];
+        int[] boxCnt = new int[9];
         for (int i = 0; i < 9; i++) {
-            List<Character> list= new ArrayList<Character>();
-            List<Character> list0= new ArrayList<Character>();
             for (int j = 0; j < 9; j++) {
-                char c = board[j][i];
-                if(c != '.'){
-                    if(list.contains(c)){
-                        return false;
-                    } else {
-                        list.add(c);
-                    }
+                if ('.' == board[i][j]) {
+                    continue;
                 }
-                c = board[i][j];
-                if(c != '.'){
-                    if(list0.contains(c)){
-                        return false;
-                    } else {
-                        list0.add(c);
-                    }
+                //处理成int型
+                int num = board[i][j] - 48;
+                // 处理行
+                if ((rowCnt[i] >> num) % 2 == 1) {
+                    return false;
+                } else {
+                    rowCnt[i] += 1 << num;
                 }
-            }
-        }
-        //校验小框
-        List<char[]> list = new ArrayList<>();
-        list.add(new char[]{0,1,2});
-        list.add(new char[]{3,4,5});
-        list.add(new char[]{6,7,8});
-        for (int i = 0; i < list.size(); i++) {
-            char[] arr1 = list.get(i);
-            for (int j = 0; j < list.size(); j++) {
-                char[] arr2 = list.get(j);
-                List<Character> list1= new ArrayList<Character>();
-                for (int k = 0; k < arr1.length; k++) {
-                    for (int l = 0; l < arr2.length; l++) {
-                        char c = board[arr1[k]][arr2[l]];
-                        if(c != '.'){
-                            if(list1.contains(c)){
-                                return false;
-                            } else {
-                                list1.add(c);
-                            }
-                        }
-                    }
+                // 处理列
+                if ((colCnt[j] >> num) % 2 == 1) {
+                    return false;
+                } else {
+                    colCnt[j] += 1 << num;
+                }
+                // 处理框
+                int boxNum = i / 3 * 3 + j / 3;
+                if ((boxCnt[boxNum] >> num) % 2 == 1) {
+                    return false;
+                } else {
+                    boxCnt[boxNum] = boxCnt[boxNum] + (1 << num);
                 }
             }
         }
---
+++ MyProcedure/src/demo/practice/Subject86.java
@@ -0,0 +1,20 @@
+package demo.practice;
+
+import java.util.List;
+
+/**
+ * 给定一个无重复元素的数组candidates和一个目标数target，找出candidates中所有可以使数字和为target的组合。
+ * candidates中的数字可以无限制重复被选取。
+ * 说明：
+ * 所有数字（包括target）都是正整数。
+ * 解集不能包含重复的组合。
+ */
+public class Subject86 {
+    public static void main(String[] args) {
+
+    }
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        return null;
+    }
+}
