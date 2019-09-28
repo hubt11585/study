051ce9f1fdd55814bb18b320f73b1f3613c9b1df
hubt11585 <1050750201@qq.com>
2019-09-28 21:03:07 +0800

    subject86

--- MyProcedure/src/demo/practice/Subject86.java
+++ MyProcedure/src/demo/practice/Subject86.java
@@ -1,5 +1,7 @@
 package demo.practice;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -10,11 +12,101 @@ import java.util.List;
  * 解集不能包含重复的组合。
  */
 public class Subject86 {
-    public static void main(String[] args) {
 
+    List<List<Integer>> resultList = null;
+
+    public static void main(String[] args) {
+        int[] candidates = new int[]{1};
+        List<List<Integer>> list = new Subject86().combinationSum(candidates,1);
+        System.out.println(list);
     }
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        return null;
+        resultList = new ArrayList<>();
+        int arrLength = candidates.length;
+        if(arrLength < 0){
+            return null;
+        }
+        // 排除大于target 的数。
+
+        List<Integer> newList = new ArrayList();
+        for (int i = 0; i < arrLength; i++) {
+            if(candidates[i] <= target){
+                newList.add(candidates[i]);
+            }
+        }
+        Collections.sort(newList);
+
+        List<List<Integer>> listTmp = new ArrayList<>();
+        this.getResultList(newList,target,listTmp);
+        return resultList;
+    }
+
+    /**
+     * target 减去整个数组，后结果大于0的保存下来，并且将小于0的数据都删除，知道newList数据为空。
+     * @param newList
+     * @param target
+     * @param listTmp
+     */
+    public void getResultList(List<Integer> newList,int target,List<List<Integer>> listTmp) {
+        if(newList.size() <= 0){
+            return;
+        }
+        List<List<Integer>> listTmp0 =  new ArrayList<>();
+        listTmp0.addAll(listTmp);
+        listTmp.clear();
+        int tmp0Sum = 0;
+        if(listTmp0.size() > 0){
+            tmp0Sum = sumList(listTmp0.get(0));
+        }
+        for (int i = 0; i < newList.size(); i++) {
+            if(target - tmp0Sum - newList.get(i) < 0){
+                newList.remove(i);
+                i--;
+                continue;
+            }
+            if(listTmp0.size() > 0) {
+                for (int j = 0; j < listTmp0.size(); j++) {
+                    int tmpSum = sumList(listTmp0.get(j));
+                    int dValue = target - tmpSum - newList.get(i);
+                    if (dValue > 0) {
+                        List<Integer> list = new ArrayList<>();
+                        list.addAll(listTmp0.get(j));
+                        list.add(newList.get(i));
+                        listTmp.add(list);
+                    } else if (dValue == 0) {
+                        List<Integer> list = new ArrayList<>();
+                        list.addAll(listTmp0.get(j));
+                        list.add(newList.get(i));
+                        Collections.sort(list);
+                        if(!resultList.contains(list)){
+                            resultList.add(list);
+                        }
+                        listTmp.add(list);
+                    }
+                }
+            }else{
+                int dValue = target - newList.get(i);
+                if(dValue > 0){
+                    List<Integer> list = new ArrayList<>();
+                    list.add( newList.get(i));
+                    listTmp.add(list);
+                }else if(dValue == 0){
+                    List<Integer> list = new ArrayList<>();
+                    list.add( newList.get(i));
+                    resultList.add(list);
+                    listTmp.add(list);
+                }
+            }
+        }
+        getResultList(newList,target,listTmp);
+    }
+
+    public int sumList(List<Integer> integerList) {
+        int sum = 0;
+        for (int i = 0; i < integerList.size(); i++) {
+            sum += integerList.get(i);
+        }
+        return sum;
     }
 }
---
+++ MyProcedure/src/demo/practice/Subject87.java
@@ -0,0 +1,20 @@
+package demo.practice;
+
+import java.util.List;
+
+/**
+ * 给定一个数组candidates和一个目标数target，找出candidates中所有可以使数字和为target的组合。
+ * candidates中的每个数字在每个组合中只能使用一次。
+ * 说明：
+ * 所有数字（包括目标数）都是正整数。
+ * 解集不能包含重复的组合。
+ */
+public class Subject87 {
+    public static void main(String[] args) {
+
+    }
+
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        return null;
+    }
+}
