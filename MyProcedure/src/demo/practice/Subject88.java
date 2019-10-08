de19c97b747c76fb21dd1cdb88737121c80435b8
AccelerateWorld <1050750201@qq.com>
2019-10-08 21:09:36 +0800

    subject88

---
+++ MyProcedure/src/demo/practice/Subject88.java
@@ -0,0 +1,14 @@
+package demo.practice;
+
+/**
+ * 给定一个未排序的整数数组，找出其中没有出现的最小的正整数。
+ */
+public class Subject88 {
+    public static void main(String[] args) {
+
+    }
+
+    public int firstMissingPositive(int[] nums) {
+        return 0;
+    }
+}
