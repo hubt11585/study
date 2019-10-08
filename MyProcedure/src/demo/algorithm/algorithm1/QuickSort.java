fec10c4a02580f68693823a61923a7476d6bd0dd
AccelerateWorld <1050750201@qq.com>
2019-10-08 21:06:56 +0800

    subject87

--- MyProcedure/src/demo/algorithm/algorithm1/QuickSort.java
+++ MyProcedure/src/demo/algorithm/algorithm1/QuickSort.java
@@ -1,5 +1,16 @@
-package demo.knowledgepoints.quicksort;
+package demo.algorithm.algorithm1;
 
+/**
+ * 给定一个数组：按照从小到大排序。
+ * 思路：
+ * 1. 获取第一个数放入临时变量data，将大于data的数放右边，小于data的数放在左边。
+ * 2. data左边和右边的数组，使用第一点的方法类推。
+ *
+ * 第一点的具体实现：
+ * 获取第一个数放入临时变量data，从右边开始遍历，获取第一个比data小的数，将这个数放入data原本的位置（位置角标为0，满足小于data的数放入
+ * data左边的逻辑），然后从左开始遍历获取第一个大于data的数，放入刚刚空出来的位置（这个位置是右边是一定大于data的，所以将此数放入该位置
+ * 满足大于data的数放右边的逻辑，再右边，再左边，直到左右重合停止，并最后将data放入最后的重合的空位中。）。
+ */
 public class QuickSort {
     public static void main(String args[]){
         int[] num={4,5,2,9,6,4,0,2,4,7};
@@ -7,6 +18,7 @@ public class QuickSort {
         for(int n:num) {
             System.out.print(n+" ");
         }
+        System.out.println();
     }
 
     public int PartSort(int arr[], int low, int high) {
@@ -27,7 +39,6 @@ public class QuickSort {
         /**循环退出后 low和high重合 将将键值赋给第low，并将low返回*/
         return low;
     }
-
 
     public void QuickSort(int arr[], int low, int high) {
         if(low<high) {
--- MyProcedure/src/demo/practice/Subject87.java
+++ MyProcedure/src/demo/practice/Subject87.java
@@ -1,6 +1,9 @@
 package demo.practice;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * 给定一个数组candidates和一个目标数target，找出candidates中所有可以使数字和为target的组合。
@@ -10,11 +13,55 @@ import java.util.List;
  * 解集不能包含重复的组合。
  */
 public class Subject87 {
-    public static void main(String[] args) {
 
+    private List<List<Integer>> res = new ArrayList<>();
+
+    public static void main(String[] args) {
+        int[] candidates = new int[]{10,1,2,7,6,1,5};
+        List<List<Integer>> list = new Subject87().combinationSum2(candidates,7);
+        System.out.println(list);
     }
 
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        return null;
+        int len = candidates.length;
+        if (len == 0) {
+            return res;
+        }
+        // 优化添加的代码1：先对数组排序，可以提前终止判断
+        Arrays.sort(candidates);
+        findCombinationSum(target, 0, new Stack<>(),candidates);
+        return res;
+    }
+
+    private void findCombinationSum(int residue, int start, Stack<Integer> pre, int[] candidates) {
+        if (residue == 0) {
+            // Java 中可变对象是引用传递，因此需要将当前 path 里的值拷贝出来
+            List list= new ArrayList<>(pre);
+            if(!res.contains(list)){
+                res.add(list);
+            }
+            return;
+        }
+        // 优化添加的代码2：在循环的时候做判断，尽量避免系统栈的深度
+        // residue - candidates[i] 表示下一轮的剩余，如果下一轮的剩余都小于 0 ，就没有必要进行后面的循环了
+        // 这一点基于原始数组是排序数组的前提，因为如果计算后面的剩余，只会越来越小
+        for (int i = start; i < candidates.length && residue - candidates[i] >= 0; i++) {
+            pre.add(candidates[i]);
+            // 【关键】因为元素可以重复使用，这里递归传递下去的是 i 而不是 i + 1
+            findCombinationSum(residue - candidates[i], i, pre,this.copyArr2(candidates,i));
+            pre.pop();
+        }
+    }
+
+    public int[] copyArr2(int[] candidatesTmp,int index){
+        int[] candidates = new int[candidatesTmp.length-1];
+        for (int i = 0,j = 0; i < candidatesTmp.length; i++) {
+            if(index == i){
+               continue;
+            }else{
+                candidates[j++] = candidatesTmp[i];
+            }
+        }
+        return candidates;
     }
 }
