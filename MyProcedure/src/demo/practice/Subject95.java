c3280a09e4525ce12661dd47738e9058d7417396
AccelerateWorld <1050750201@qq.com>
2019-11-09 16:33:10 +0800

    subject95

--- MyProcedure/src/demo/practice/Subject95.java
+++ MyProcedure/src/demo/practice/Subject95.java
@@ -9,9 +9,20 @@ package demo.practice;
 public class Subject95 {
     public static void main(String[] args) {
         int[][] arrInt = new int[][]{{1,2,3},{4,5,6},{7,8,9}};
+        new Subject95().rotate(arrInt);
+        System.out.println(arrInt);
     }
 
     public void rotate(int[][] matrix) {
-
+        int n = matrix.length;
+        for (int i = 0; i < (n + 1) / 2; i ++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
+                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 -i];
+                matrix[j][n - 1 - i] = matrix[i][j];
+                matrix[i][j] = temp;
+            }
+        }
     }
 }
