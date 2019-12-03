2821d6df248a94fd5cecd8e1238496cbc98a93c9
AccelerateWorld <1050750201@qq.com>
2019-12-03 20:23:15 +0800

    subject96

--- MyProcedure/src/demo/practice/Subject96.java
+++ MyProcedure/src/demo/practice/Subject96.java
@@ -1,6 +1,11 @@
 package demo.practice;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。
@@ -18,11 +23,39 @@ import java.util.List;
  */
 public class Subject96 {
     public static void main(String[] args) {
-        new Subject96().groupAnagrams(null);
-
+        String[] strArr = new String[]{"eat", "tea", "tan", "ate", "nat", "bat"};
+        List<List<String>> listList = new Subject96().groupAnagrams(strArr);
+        System.out.println(listList);
     }
 
+    List<List<String>> listList = new ArrayList<>();
+    Map<String,List<String>> map = new HashMap<>();
     public List<List<String>> groupAnagrams(String[] strs) {
-        return null;
+        for (int i = 0; i < strs.length; i++) {
+            char[] ch = strs[i].toCharArray();
+            String str = dealChar(ch);
+            if(map.get(str) == null){
+                List<String> list = new ArrayList<>();
+                list.add(strs[i]);
+                map.put(str,list);
+            }else{
+                List<String> list = map.get(str);
+                list.add(strs[i]);
+            }
+        }
+
+        Iterator<Map.Entry<String, List<String>>> it=map.entrySet().iterator();
+        while(it.hasNext()){
+            Map.Entry<String, List<String>> entry=it.next();
+            listList.add(entry.getValue());
+        }
+
+        return listList;
+    }
+
+    public String dealChar(char[] ch) {
+        String str = "";
+        Arrays.sort(ch);
+        return new String(ch,0,ch.length);
     }
 }
---
+++ MyProcedure/src/demo/practice/Subject97.java
@@ -0,0 +1,12 @@
+package demo.practice;
+
+/**
+ * 实现 pow(x, n) ，即计算 x 的 n 次幂函数。
+ * -100.0 < x < 100.0
+ * n 是 32 位有符号整数，其数值范围是 [?2^31, 2^31 ? 1] 。
+ */
+public class Subject97 {
+    public double myPow(double x, int n) {
+        return 0;
+    }
+}
