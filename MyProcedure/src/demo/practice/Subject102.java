69831eff6a592e367f86365a58f89ba25d77f9c8
AccelerateWorld <1050750201@qq.com>
2020-01-14 15:55:20 +0800

    subject102

--- MyProcedure/src/demo/practice/Subject102.java
+++ MyProcedure/src/demo/practice/Subject102.java
@@ -7,7 +7,22 @@ package demo.practice;
  */
 public class Subject102 {
 
+    public static void main(String[] args) {
+        int[] arr = new int[]{3,2,1,0,1};
+        System.out.println(new Subject102().canJump(arr));
+    }
+
     public boolean canJump(int[] nums) {
-        return false;
+        int lengths =  nums.length;
+        if(lengths <= 1) return true;
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if(i+nums[i] > max && i <= max){
+                max = i+nums[i];
+            }else if(nums[i] == 0 && max < i+1 && lengths-1 != i){
+                return false;
+            }
+        }
+        return true;
     }
 }
---
+++ MyProcedure/src/demo/practice/Subject103.java
@@ -0,0 +1,11 @@
+package demo.practice;
+
+/**
+ * 给出一个区间的集合，请合并所有重叠的区间。
+ */
+public class Subject103 {
+
+    public int[][] merge(int[][] intervals) {
+        return null;
+    }
+}
