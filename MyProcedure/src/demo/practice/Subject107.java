866b13e69b4c99fffd1967870f052a48855b5af6
AccelerateWorld <1050750201@qq.com>
2020-05-29 17:01:25 +0800

    subject107

---
+++ MyProcedure/src/demo/practice/Subject107.java
@@ -0,0 +1,23 @@
+package demo.practice;
+
+/**
+ * 给出集合?[1,2,3,…,n]，其所有元素共有 n! 种排列。
+ * 按大小顺序列出所有排列情况，并一一标记，当 n = 3 时, 所有排列如下：
+ * "123"
+ * "132"
+ * "213"
+ * "231"
+ * "312"
+ * "321"
+ * 给定n 和k，返回第k个排列。
+ */
+public class Subject107 {
+
+    public static void main(String[] args) {
+        new Subject107().getPermutation(1,2)
+    }
+
+    public String getPermutation(int n, int k) {
+        return "";
+    }
+}
